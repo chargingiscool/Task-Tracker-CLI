19511c21b9e1c75199bf488e7beeec280a9a66c4
chargingiscool <162662310+chargingiscool@users.noreply.github.com>
2025-04-28 07:53:00 -0700

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,6 +1,28 @@
 import json
+import datetime
+
+def add_task():
+    name = input("Task name: ")
+    desc = input("Describe task: ")
+    status = input("Status: ")
+
+    new_task = {
+        "name": f"{name}",
+        "desc": f"{desc}",
+        "status": f"{status}",
+        "createdAt": f"{str(datetime.datetime.now())[:-10]}",
+    }
+
+    return new_task
+
 with open('data.json','r') as file:
     data = json.load(file)
 
-    
+
+data.append(add_task())
+
+
+
+with open('data.json','w') as file:
+    json.dump(data,file,indent=2)
 
